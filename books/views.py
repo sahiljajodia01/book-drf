ba79d19187f0d319a002ed5ca020853045e48ebc
Sahil Jajodia <sahil.jajodia@gmail.com>
2017-10-28 00:26:13 +0530

    Added viewsets

--- books/views.py
+++ books/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Author, Book, Publisher
+from .serializers import AuthorSerializer, BookSerializer, PublisherSerializer
 
-# Create your views here.
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class PublisherViewSet(viewsets.ModelViewSet):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
